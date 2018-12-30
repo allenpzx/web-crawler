a3ded2db374dd1f359a780e464b1a5e84921e39c
彭子修 <pengzixiu@UNKNOWN-64-58-79-X.yahoo.com>
2018-12-30 16:04:56 +0800

    ok

--- server/server.js
+++ server/server.js
@@ -1,87 +1,31 @@
 const express = require('express');
 const app = express();
 const port = 9090;
-const request = require('superagent');
-const cheerio = require('cheerio');
-const url = require('url');
-const async = require("async");
-const nodeUrl = 'http://cnodejs.org';
+const path = require('path');
+// const request = require('superagent');
+// const cheerio = require('cheerio');
+// const url = require('url');
+// const async = require("async");
+// const nodeUrl = 'http://cnodejs.org';
 
-app
-.use(express.static(path.join(__dirname, 'public')));
-
-
-
-app.get('/topics', (req, res) => {
-    request
-        .get('https://cnodejs.org/api/v1/topics')
-        .then(response => {
-            let topics = [];
-            const data = response.body.data;
-            data.map(v => {
-                const items = [];
-                const $ = cheerio.load(v.content);
-                $('a').each(function (idx, element) {
-                    const ele = $(element)
-                    console.log(ele);
-                    ele.attr('href').includes('https://cnodejs.org') ?
-                        items.push(ele.attr('href')) :
-                        null
-                })
-
-                topics.push({
-                    title: v.title,
-                    author: v.author.loginname,
-                    visit_count: v.visit_count,
-                    reply_count: v.reply_count
-                })
-            })
-            res.send(topics)
-        })
-        .catch(err => {
-            console.log('error: ', err)
-            res.send(err, err.message, err.response)
-        })
-})
-
-app.get('/posts', (req, res) => {
-    request
-        .get('https://cnodejs.org')
-        .then(response => {
-            const $ = cheerio.load(response.text);
-            let items = [];
-            const baseUrl = 'http://cnodejs.org';
-            $('#topic_list .topic_title').each(function (idx, element) {
-                var $element = $(element);
-                items.push({
-                    title: $element.attr('title'),
-                    href: baseUrl + $element.attr('href')
-                });
-            });
-            res.send(items)
-        })
-        .catch(err => {
-            console.log('error: ', err)
-            res.send(err, err.message, err.response)
-        })
-})
-
-app.get('/', async function (req, res){
-        try{
+    app
+    .use(express.static(path.join(__dirname, 'public')))
+    .get('/', function (req, res) {
+        // try {
 
             // get post's title and link
-            const index_page = await request.get('https://cnodejs.org');
-            const $ = cheerio.load(index_page.text);
-            let post_list = [];
-            $('#topic_list .topic_title').each(function (idx, element) {
-                const ele = $(element);
-                post_list.push({
-                    title: ele.attr('title'),
-                    href: url.resolve(nodeUrl, ele.attr('href'))
-                });
-            });
+            // const index_page = await request.get('https://cnodejs.org');
+            // const $ = cheerio.load(index_page.text);
+            // let post_list = [];
+            // $('#topic_list .topic_title').each(function (idx, element) {
+            //     const ele = $(element);
+            //     post_list.push({
+            //         title: ele.attr('title'),
+            //         href: url.resolve(nodeUrl, ele.attr('href'))
+            //     });
+            // });
 
-            let post_detail = [];
+            // let post_detail = [];
 
             // async.mapLimit(post_list, 5, function(item, callback){
             //     return request
@@ -97,10 +41,10 @@ app.get('/', async function (req, res){
 
             res.send('ceshi');
 
-        }catch(error){
-            throw error
-        }
-})
+        // } catch (error) {
+            // throw error
+        // }
+    })
 
 app.listen(port, (error) => {
     if (error) {
