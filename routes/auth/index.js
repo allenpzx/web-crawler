f0575f39821c48f7d60aa9d0db6e98820dacd9b3
彭子修 <pengzixiu@pengzixiudeMacBook-Air.local>
2019-01-07 17:03:44 +0800

    save

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -1,25 +1,26 @@
 const express = require('express');
 const router = express.Router();
 
-// const mysql = require('mysql');
-// const connection = mysql.createConnection({
-//   host     : 'localhost',
-//   user     : 'root',
-//   password : 'moremore'
-// });
-// connection.connect(function(err) {
-//   if (err) {
-//     console.error('error connecting: ' + err.stack);
-//     return;
-//   }
-//   console.log('connected as id ' + connection.threadId);
-
-// });
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'moremore'
+});
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+  console.log('connected as id ' + connection.threadId);
+});
 
 router.get('/*', function (req, res, next){
     const cookies = req.cookies;
+    // console.log(req.cookies);
     if(!cookies){
-        res.render('auth/login.ejs')
+        res.render('auth/login.ejs');
+        return
     }
     if(req.url === '/'){
         return res.render('index/index.pug', {title: 'crawler test'});
@@ -28,23 +29,27 @@ router.get('/*', function (req, res, next){
 });
 
 router.get('/login', function (req, res, next){
-    if(req.cookies){
-        res.render('index')
-    }
-    res.render('auth/login')
+    // if(req.cookies){
+    //     res.render('index/index.pug')
+    //     return 
+    // }
+    res.render('auth/login.ejs')
 });
 router.get('/register', function (req, res, next){
-    if(req.cookies){
-        res.render('index')
-    }
+    // if(req.cookies){
+    //     res.render('index/index.pug')
+    //     return 
+    // }
     res.render('auth/register')
 });
 
 router.post('/login', function (req, res, next){
-    console.log(req.body)
+    const {account, password} = req.body;
+
+    console.log('req.body: ', req.body);
 });
 router.post('/register', function (req, res, next){
-    console.log(req.body)
+    console.log('req.body: ', req.body);
 });
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -5,8 +5,13 @@ const path = require('path');
 const authRouter = require(path.resolve(__dirname, '../routes/auth/index.js'));
 const cnodeRouter = require(path.resolve(__dirname, '../routes/cnode/index.js'));
 const dataDrivenRouter = require(path.resolve(__dirname, '../routes/data-driven/index.js'));
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 app
+.use(bodyParser.urlencoded({ extended: false }))
+.use(bodyParser.json())
+.use(cookieParser())
 .use(express.static(path.join(__dirname, '../public')))
 .set('views', path.join(__dirname, '../views'))
 .set('view engine', 'pug')
